10588f7c764a84df4c4472dec529ae77d41362ac
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2023-11-30 17:14:33 +0100

    Removed old modules src and Engine

--- androidEngine/build.gradle.kts
+++ androidEngine/build.gradle.kts
@@ -32,8 +32,6 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    implementation(project(mapOf("path" to ":Engine")))
-    implementation(project(mapOf("path" to ":src")))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
